1c83ecc18af890924f2869a88982b5863c2c87d4
nasreddinebr <berrachednasr@gmail.com>
2017-08-29 17:05:05 +0200

    Ajout du fichier mots.js

---
+++ mots.js
@@ -0,0 +1,15 @@
+var mots = [], motSaisie = " ";
+while (motSaisie !== "stop") {
+	motSaisie = window.prompt("Entrer un mot ou taper stop pour quitter:");
+	if (motSaisie === "stop") {
+		console.log("A biento!!!");
+	} else {
+		mots.push(motSaisie);
+	}
+	
+}
+
+console.log("Vous avez saisie les mots suivants:");
+mots.forEach(function (mot) {
+	console.log(mot);
+});

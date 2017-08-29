5acc8e503c81c644d05deae8bfe3dc842f1f0d43
nasreddinebr <berrachednasr@gmail.com>
2017-08-29 17:08:52 +0200

    Effacement du (;) a la ligne 5 pour provoquer une erreur

--- project1.java
+++ project1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class project1 {
 
 	public static void main(String[] args) {
-		char reponse = ' ';
+		char reponse = ' '
 		double result, temperatur;
 		String x1, x2;
 		
